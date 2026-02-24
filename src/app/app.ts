f9358ca697e1ac50d3e71e47a5f33f58810e1053
young <stupid80096@gmail.com>
2026-02-25 00:33:14 +0800

    feat: update console warning

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,7 +10,7 @@ import { ScratchCard } from './scratch-card/scratch-card';
 export class App {
 	constructor() {
 		console.log(
-			'%c抓到！好色哦！你竟然想偷看！',
+			'%c抓到！好色哦！你竟然想偷看！\nMen! What can i say?\n嚴厲斥責你發瘋啦 這什麼東西啦不可以啊 不要再亂搞了啦',
 			'color: white; background-color: #ff6699; padding: 2px 6px; border-radius: 4px; font-weight: bold; font-size: 48px'
 		);
 	}
